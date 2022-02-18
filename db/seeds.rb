1946b7869a9c3071ee426bd0f430eef77fa22292
Clare Amos <clare.amos@gmail.com>
2022-02-18 08:49:32 +0000

    added first seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,58 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "creating quizzes"
+puts "creating questions"
+puts "creating answers"
+puts "creating beasts"
+Beast.destroy_all
+Quiz.destroy_all
+Question.destroy_all
+Answer.destroy_all
+puts "Database cleaned"
+
+Quiz.create!(
+  name: 'Beastly Drag Queen Generator')
+
+Question.create!(
+  quiz_id: quiz.first.id,
+  question: "What's your favourite colour?")
+
+Question.create!(
+  quiz_id: quiz.first.id,
+  question: "What's your favourite element?")
+
+Question.create!(
+   quiz_id: quiz.first.id,
+  question: "What's your favourite music genre?")
+
+Answer.create!(
+  letter: "A."
+  answer: "Blackest midnight of the soul"
+  beast_id:
+  question_id: question.first.id)
+
+Answer.create!(
+  letter: "B."
+  answer: "Tequila Sunrise"
+  beast_id:
+  question_id:question.first.id)
+
+Answer.create!(
+  letter: "C."
+  answer: "Poison Ivy"
+  beast_id:
+  question_id: question.first.id)
+
+Answer.create!(
+  letter: "D."
+  answer: "Angel wings and unicorn's breath"
+  beast_id:
+  question_id: question.first.id)
+
+
+
+
+
+
+
