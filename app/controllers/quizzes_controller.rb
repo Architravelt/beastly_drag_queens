3aede878aac1844bfc0ca255703a9cf9c733a1ac
Clare Amos <clare.amos@gmail.com>
2023-05-13 17:15:59 +0100

    controller actions for choices

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,4 +1,13 @@
 class QuizzesController < ApplicationController
 
+  def bdqQuiz
+    @bdq_questions = Question.all
+    @emptyAnswers = []
+
+    Question.all.each do
+      @emptyAnswers << Answer.new
+    end
+  end
+
 
 end
