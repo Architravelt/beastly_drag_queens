f8e5597f49e6923aaac7c132df20efabbf6a8069
Clare Amos <clare.amos@gmail.com>
2023-06-06 17:51:33 +0100

    radio button form submit

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,6 +6,15 @@ class QuestionsController < ApplicationController
 
   end
 
+  def choices
+    @questions = Question.all
+    @emptyChoices = []
+
+    @questions.each do
+      @emptyChoices << Choice.new
+    end
+  end
+
   # def first
   #   @question.first = Question.find_by(name: "one").id
   # end
